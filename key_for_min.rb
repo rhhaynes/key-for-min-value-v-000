c2063bf04c6539f802a5e4a8b41090d3c4862357
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-26 14:05:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 def key_for_min_value(name_hash)
   return nil if name_hash.empty?
-  name,num = name_hash.first
-  name_hash.each { |k,v| name,num = k,v if num<v }
-  name
+  key,val = name_hash.first
+  name_hash.each { |k,v| key,val = k,v if val<v }
+  key
 end
