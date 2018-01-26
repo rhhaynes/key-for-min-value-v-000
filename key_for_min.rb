f59c3306f39e1370e85be87f8208138c20f42c09
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-26 14:07:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
+require 'pry'
+
 def key_for_min_value(name_hash)
   return nil if name_hash.empty?
   key,val = name_hash.first
-  name_hash.each { |k,v| key,val = k,v if val<v }
+  name_hash.each { |k,v| key,val = k,v if val<v; binding.pry}
   key
 end
